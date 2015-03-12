283c52b67fa11908f0d317f66b683004f64c989c
Jason Noer <jnoer@widen.com>
2015-03-12 16:32:53 -0500

    gallery page, photo page.

--- web/javascript/app.js
+++ web/javascript/app.js
@@ -36,7 +36,16 @@ appModule.config(['$routeProvider', function ($routeProvider) {
             templateUrl: 'photography.html'
         })
         .when('/galleries', {
-            templateUrl: 'galleries.html'
+            templateUrl: 'galleries.html',
+            controller: 'galleriesCtrl'
+        })
+        .when('/gallery/:galleryName', {
+            templateUrl: 'gallery.html',
+            controller: 'galleryCtrl'
+        })
+        .when('/photo/:filename', {
+            templateUrl: 'photo.html',
+            controller: 'photoCtrl'
         });
     //.otherwise({
     //    redirectTo: '/'
@@ -46,3 +55,41 @@ appModule.config(['$routeProvider', function ($routeProvider) {
 appModule.controller('mainController', ['$scope', '$location', function ($scope, $location) {
     $scope.location = $location;
 }]);
+
+//appModule.controller('galleriesCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
+appModule.controller('galleriesCtrl', ['$scope', '$http', function ($scope, $http) {
+    $http.get('galleries.json').success(function(data) {
+       $scope.galleries = data;
+    });
+}]);
+
+appModule.controller('galleryCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $scope.galleryName = $routeParams.galleryName;
+
+    $scope.getThumb = function(photo) {
+        return photo.filename.replace('.JPG', '_thumb.JPG');
+    };
+
+    $http.get('photos.json').success(function(data) {
+        $scope.photos = data.filter(function(item){
+            return item.gallery === $routeParams.galleryName;
+        });
+    });
+}]);
+
+appModule.controller('photoCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+    //console.log('route: ' + $routeParams.filename);
+
+    $http.get('photos.json').success(function(data) {
+        $scope.photo = data.filter(function(item) {
+            //console.log(item.filename);
+            return item.filename === $routeParams.filename;
+        })[0];
+    });
+
+    console.log($scope.photo);
+
+    //$scope.photo = $routeParams.fileName;
+}]);
+
+
